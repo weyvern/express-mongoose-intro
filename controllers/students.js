2d89b989ce4d967a261c53d5348b974c5d0b0b07
Jorge Paul <jserranopaul@gmail.com>
2021-04-01 15:44:48 +0200

    GET all, GET single, POST

--- controllers/students.js
+++ controllers/students.js
@@ -1,5 +1,39 @@
-export const getAllStudents = async (req, res) => res.send('Student');
-export const getSingleStudent = async (req, res) => {};
-export const createNewStudent = async (req, res) => {};
+import Student from '../models/Student.js';
+
+export const getAllStudents = async (req, res) => {
+  try {
+    const students = await Student.find();
+    res.json(students);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getSingleStudent = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const student = await Student.findById(id);
+    if (!student) return res.status(404).json({ message: `Student with id of ${id} not found` });
+    res.json(student);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const createNewStudent = async (req, res) => {
+  try {
+    const { first_name, last_name, email, password } = req.body;
+    const newStudent = await Student.create({
+      first_name,
+      last_name,
+      email,
+      password
+    });
+    res.status(201).json(newStudent);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 export const updateStudent = async (req, res) => {};
 export const removeStudent = async (req, res) => {};
